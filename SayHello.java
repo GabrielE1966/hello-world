2fbf63be1b6a42516eae9927dc8d2830c4b34726
Elizedek Gabriel <gelizdek@yahoo.com>
2024-11-16 22:46:36 +0200

    add hello world file

---
+++ SayHello.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class SayHello here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class SayHello
+{
+
+    /**
+     * Constructor for objects of class SayHello
+     */
+    public SayHello()
+    {
+    }
+
+    public static void main(String []args) {
+        System.out.println("Hello");
+    }
+}
